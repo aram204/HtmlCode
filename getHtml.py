cd8f7c89c29fbf37588c411dade5461b9d8caf29
aram2004 <arsarg6440@gmail.com>
2020-07-01 20:34:26 +0400

    HtmlCode

--- getHtml.py
+++ getHtml.py
@@ -1,9 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
 html = requests.get("https://httpstatuses.com/")
 a = html.content.decode("utf-8")
-print(a)
 with open('code.html','w') as f:
     f.write(a)
+soup = BeautifulSoup(html.text)
+for tag in soup.find_all("li"):
+    print(tag.text)
 
 
 
